5a9efeab1a4299b57b30895e4f601496c4ec5c8f
Shelley Ophir <qidcoder@gmail.com>
2020-11-24 05:58:36 -0500

    Added click to render

--- tabs/src/components/Tabs.js
+++ tabs/src/components/Tabs.js
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Tabs = props => {
     const { tab_list } = props;
-
-
-
+    const [selectedIdx, setSelectedIdx] = useState(0);
 
     return (
         <div style={{display: 'flex', flexWrap: 'wrap'}}>
             {
                 tab_list.map((item, i) => {
                     return (
-                        <div key={i} style={{width: '300px', border: '1px solid black', margin: '4px', padding: '4px'}}>
+                        <div key={i} style={{width: '300px', border: '1px solid black', margin: '4px', padding: '4px'}} onClick={e =>{setSelectedIdx(i)}}>
                             {item.tab_name}
                         </div>
                     )
                 })
             }
+
+            <div style={{width: '1000px', border: '1px solid black', margin: '4px', padding: '4px'}}>
+                {tab_list[selectedIdx].tab_content}
+            </div>
         </div>
 
     )
